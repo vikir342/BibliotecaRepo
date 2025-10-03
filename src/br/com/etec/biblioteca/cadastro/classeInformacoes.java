c2fa14cb1f168c3b4a2ef9375be25cbdafd67e53
vikir342 <vikiretec@gmail.com>
2025-10-03 10:42:03 -0300

    adicionado

--- src/br/com/etec/biblioteca/cadastro/classeInformacoes.java
+++ src/br/com/etec/biblioteca/cadastro/classeInformacoes.java
@@ -1,8 +1,45 @@
 package br.com.etec.biblioteca.cadastro;
 
-public class classeInformacoes {
+	public class ClasseInformacoes {
 
-	//atributo
-	String TitulosAt;
-	String 
-}
+
+		//Atributo
+		String titulosAt;
+		String subtitulosAt;
+		int numerosPaginasAt;
+		String autoresAt;
+		String generosAt;
+		
+		//Construtor vazio
+		public ClasseInformacoes(){
+			
+		}
+		
+		//Construtor vazio
+		public ClasseInformacoes(String autor, String titulo){
+			this.autoresAt = autor;
+			this.titulosAt = titulo;
+		}
+		
+		//Getters
+		public String getTitulo(){
+			return titulosAt;
+		}
+		
+		public String getSubTitulo(){
+			return subtitulosAt;
+		}
+		
+		public String getAutores(){
+			return autoresAt;
+		}
+		
+		public String getGeneros(){
+			return generosAt;
+		}
+		
+		public int getnumeroPaginas(){
+			return numerosPaginasAt;
+		}
+		
+	}
